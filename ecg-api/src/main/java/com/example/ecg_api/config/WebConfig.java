c72c89a6cf5b7ce36771b6829c6b5594b0a88eab
Masahiro Tatsuta <masalowsearch@gmail.com>
2026-04-30 15:21:14 +0900

    Fix: NextAuth

--- ecg-api/src/main/java/com/example/ecg_api/config/WebConfig.java
+++ ecg-api/src/main/java/com/example/ecg_api/config/WebConfig.java
@@ -10,10 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:3000",      // ローカル開発用
-                    "https://digital-pulse-psi.vercel.app" // ★VercelのURL（後で書き換え）
-                )
+                .allowedOriginPatterns("https://*.vercel.app", "http://localhost:[*]")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
